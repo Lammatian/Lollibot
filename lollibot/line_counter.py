b0780b4da3897ce571273c9d08c5a09436e5af7c
Ignas Kancleris <ikancleris@gmail.com>
2018-02-04 13:07:47 +0000

    Minor improvements to line counter

--- lollibot/line_counter.py
+++ lollibot/line_counter.py
@@ -12,29 +12,29 @@ class LineCounter(object):
 
     def input(self, value):
         scaled_value = tuple(map(lambda r, s: r / s, value, self.max_input))
-        self.previous_inputs.append(scaled_value)
+        value_binary = sum(scaled_value) / 3 >= WHITE_THRESHOLD
+        self.previous_inputs.append(value_binary)
 
-    def smoothed_input(self, end_index):
+    def __smoothed_input(self, end_index):
         if end_index < SMOOTHING_FACTOR:
             return False
 
         start_index = end_index - SMOOTHING_FACTOR
 
         values = self.previous_inputs[start_index:end_index]
-        values_binary = [sum(v)/3 >= WHITE_THRESHOLD for v in values]
 
-        most_common = sum(values_binary) > (SMOOTHING_FACTOR / 2)
+        most_common = sum(values) > (SMOOTHING_FACTOR / 2)
 
         return most_common
 
     def last_value(self):
-        return self.smoothed_input(len(self.previous_inputs))
+        return self.__smoothed_input(len(self.previous_inputs))
 
     def count_lines(self):
         last_value = False
         switches = 0
         for i in range(1, len(self.previous_inputs)):
-            new_value = self.smoothed_input(i)
+            new_value = self.__smoothed_input(i)
             if new_value and not last_value:
                 switches += 1
 
