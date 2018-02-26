d24fe77f5f741e71fcceeefeec4093ed6d5dad3b
Lammatian <mateusz.parafinski@gmail.com>
2018-02-26 11:19:40 +0000

    Brought back encoding to messages sent

--- lollibot/bluetooth.py
+++ lollibot/bluetooth.py
@@ -73,5 +73,5 @@ class BluetoothCommunicator:
     
     def send_data(self, data):
         logger.debug("Sending {} to the client".format(data))
-        self.client_sock.send(data)
+        self.client_sock.send(data.encode())
         logger.info("Data successfully sent")
