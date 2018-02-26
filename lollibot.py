e42497c697b7463e87b755e210453f60530157e6
Ignas Kancleris <ikancleris@gmail.com>
2018-02-26 13:01:17 +0000

    Run back and forth

--- lollibot.py
+++ lollibot.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 import lollibot.movement.movement_control as movement_control
+from time import sleep
 
 if __name__ == '__main__':
     mc = movement_control.MovementControl()
-    mc.move_lines(3)
+    mc.move_lines(4)
+
+    sleep(5)
+
+    mc.move_lines(4, -1)
