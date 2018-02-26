799148efc136086cbfdddf6bc88283a91a9dbccc
Ignas Kancleris <ikancleris@gmail.com>
2018-02-26 14:22:17 +0000

    Shout exterminate

--- lollibot.py
+++ lollibot.py
@@ -2,11 +2,19 @@
 
 import lollibot.movement.movement_control as movement_control
 from time import sleep
+from ev3dev.ev3 import Sound
 
 if __name__ == '__main__':
-    mc = movement_control.MovementControl()
-    mc.move_lines(4)
+    while True:
+        word = Sound.speak("Exterminate")
 
-    sleep(5)
+        mc = movement_control.MovementControl()
+        mc.move_lines(4, 0.25)
 
-    mc.move_lines(4, -1)
+        word.wait()
+        sleep(1)
+
+        mc.move_lines(4, -0.25)
+
+        sleep(1)
+
