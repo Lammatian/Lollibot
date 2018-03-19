7045570ed52e116093d544cb91546a095b685b5a
Ignas Kancleris <ikancleris@gmail.com>
2018-03-19 13:36:48 +0000

    Set date on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ echo "Uploading files"
 
 scp -rp ${files[@]} robot@ev3dev.local:~/
 
+echo "Setting the current date to $(date)"
+
+ssh robot@ev3dev.local "echo maker | sudo -kS date -s '$(date)'"
+
 echo "Run lollibot.py on Brickman to start the application"
